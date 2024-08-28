5c0f7e64dda55c49ebbd55da247efb550d889833
Joseph Hobbs <josephrosshobbs@gmail.com>
2024-08-28 11:33:36 -0400

    Update stylesheet

---
+++ html/script.js
@@ -0,0 +1,13 @@
+// Select the tooltip by class
+var tooltip = document.querySelectorAll('.tooltip');
+
+// Move the tooltip with the mouse
+document.addEventListener('mousemove', fn, false);
+
+// Event listener
+function fn(e) {
+    for (var i=tooltip.length; i--;) {
+        tooltip[i].style.left = e.pageX + 'px';
+        tooltip[i].style.top = e.pageY + 'px';
+    }
+}
